ff1d2448c9cbeb1ed66442787d8bb74c806d0675
vnaysngh <singhvinay2525@gmail.com>
2024-12-24 00:44:05 +0530

    employer profile details page updated

--- src/app/components/candidate-details/work-experience.tsx
+++ src/app/components/candidate-details/work-experience.tsx
@@ -63,6 +63,19 @@ const WorkExperience = ({ user }: { user: any }) => {
     );
   };
 
+  const getFoundedDate = (unixTimestamp: number) => {
+    const date = new Date(unixTimestamp * 1000); // Convert to milliseconds
+
+    // Extract components
+    const day = date.getDate();
+    const month = date.toLocaleString("en-US", { month: "long" }); // Get full month name
+    const year = date.getFullYear();
+
+    // Format the date
+    const formattedDate = `${day} ${month} ${year}`;
+    return formattedDate;
+  };
+
   return (
     <div className="profile-container">
       {/* Header Section */}
@@ -78,7 +91,11 @@ const WorkExperience = ({ user }: { user: any }) => {
         </div>
         <div className="profile-title">
           <h1>{user.name}</h1>
-          <p>{user?.role}</p>
+          <p>
+            {user.user_type === "talent"
+              ? user?.role
+              : user.company_description}
+          </p>
           <div className="profile-badges">
             <span className="badge">
               <span>💳</span> {user?.address}
@@ -98,37 +115,45 @@ const WorkExperience = ({ user }: { user: any }) => {
           {/* Bio Section */}
           <section className="card">
             <h2>About</h2>
-            <p>{user.bio}</p>
+            <p>
+              {user.user_type === "talent"
+                ? user.bio
+                : user.company_details?.about}
+            </p>
           </section>
 
           {/* Experience Section */}
-          <section className="card">
-            <h2>Experience</h2>
-            {experiences?.map((exp: any, index: number) => {
-              const role: string = exp?.role;
-              return (
-                <div key={index} className="experience-item">
-                  <h3>{roles[role as keyof typeof roles]}</h3>
-                  <div className="d-flex align-items-center gap-3">
-                    <Link
-                      href={`/${exp.employer}.employd.eth`}
-                      target="_blank"
-                      className="on-hover-underline"
-                    >
-                      <p className="company text-capitalize">{exp.employer}</p>
-                    </Link>
-                    <p className="period">
-                      {exp.startMonth}/{exp.startYear} - {exp.endMonth}/
-                      {exp.endYear}
-                    </p>
-                  </div>
+          {user.user_type === "talent" && (
+            <section className="card">
+              <h2>Experience</h2>
+              {experiences?.map((exp: any, index: number) => {
+                const role: string = exp?.role;
+                return (
+                  <div key={index} className="experience-item">
+                    <h3>{roles[role as keyof typeof roles]}</h3>
+                    <div className="d-flex align-items-center gap-3">
+                      <Link
+                        href={`/${exp.employer}.employd.eth`}
+                        target="_blank"
+                        className="on-hover-underline"
+                      >
+                        <p className="company text-capitalize">
+                          {exp.employer}
+                        </p>
+                      </Link>
+                      <p className="period">
+                        {exp.startMonth}/{exp.startYear} - {exp.endMonth}/
+                        {exp.endYear}
+                      </p>
+                    </div>
 
-                  <p>{exp.description}</p>
-                  {getAttestationBadge(exp.attestationStatus)}
-                </div>
-              );
-            })}
-          </section>
+                    <p>{exp.description}</p>
+                    {getAttestationBadge(exp.attestationStatus)}
+                  </div>
+                );
+              })}
+            </section>
+          )}
 
           {/* Portfolio Section */}
           {/* <section className="card">
@@ -146,46 +171,121 @@ const WorkExperience = ({ user }: { user: any }) => {
 
         {/* Sidebar */}
         <aside>
-          <div className="card skills-container">
-            <h2>Skills</h2>
-            {user?.skills && user?.skills?.length ? (
-              <div className="skills-grid">
-                {user?.skills?.map(
-                  (skill: { label: string; value: string }, index: number) => (
-                    <span key={index} className="skill-tag">
-                      {skill.label}
-                    </span>
-                  )
-                )}
-              </div>
-            ) : (
-              <p>User has not added skills</p>
-            )}
+          {user.user_type === "talent" ? (
+            <div className="card skills-container">
+              <h2>Skills</h2>
+              {user?.skills && user?.skills?.length ? (
+                <div className="skills-grid">
+                  {user?.skills?.map(
+                    (
+                      skill: { label: string; value: string },
+                      index: number
+                    ) => (
+                      <span key={index} className="skill-tag">
+                        {skill.label}
+                      </span>
+                    )
+                  )}
+                </div>
+              ) : (
+                <p>User has not added skills</p>
+              )}
 
-            <div className="mt-30">
-              <h2>Socials</h2>
-              <div className="social-links">
-                {user?.socials?.twitter ? (
-                  <Link
-                    href={user?.socials?.twitter}
-                    className="social-link"
-                    target="_blank"
-                  >
-                    <i className="bi bi-twitter"></i>
-                  </Link>
-                ) : null}
-                {user?.socials?.linkedIn ? (
-                  <Link
-                    href={user?.socials?.linkedIn}
-                    className="social-link"
-                    target="_blank"
-                  >
-                    <i className="bi bi-linkedin"></i>
-                  </Link>
-                ) : null}
+              <div className="mt-30">
+                <h2>Social</h2>
+                <div className="social-links">
+                  {user?.socials?.twitter ? (
+                    <Link
+                      href={user?.socials?.twitter}
+                      className="social-link"
+                      target="_blank"
+                    >
+                      <i className="bi bi-twitter"></i>
+                    </Link>
+                  ) : null}
+                  {user?.socials?.linkedIn ? (
+                    <Link
+                      href={user?.socials?.linkedIn}
+                      className="social-link"
+                      target="_blank"
+                    >
+                      <i className="bi bi-linkedin"></i>
+                    </Link>
+                  ) : null}
+                </div>
               </div>
             </div>
+          ) : (
+            <div className="card">
+              <h2>Company Details</h2>
+              <div className="meta-data-list">
+                {/* <div className="meta-data-item">
+                  <span className="meta-data-label">Location:</span>
+                  <div className="meta-data-content">
+                    {employer.location.city}, {employer.location.country}
+                  </div>
+                </div> */}
+
+                <div className="meta-data-item">
+                  <span className="meta-data-label">Size:</span>
+                  <div className="meta-data-content">
+                    {user.company_details?.company_size || "-"}
+                  </div>
+                </div>
+
+                <div className="meta-data-item">
+                  <span className="meta-data-label">Founded:</span>
+                  <div className="meta-data-content">
+                    {getFoundedDate(user.company_details?.founded_date) || "-"}
+                  </div>
+                </div>
+                {/* 
+        <div className="meta-data-item">
+          <span className="meta-data-label">Phone:</span>
+          <div className="meta-data-content">
+            {employer.contact.phones.map((phone, index) => (
+              <React.Fragment key={phone}>
+                <a href={`tel:${phone.replace(/[^\d+]/g, '')}`}>{phone}</a>
+                {index < employer.contact.phones.length - 1 && ", "}
+              </React.Fragment>
+            ))}
           </div>
+        </div>
+ */}
+                <div className="meta-data-item">
+                  <span className="meta-data-label">Category:</span>
+                  <div className="meta-data-content">
+                    {user.company_details?.category || "-"}
+                  </div>
+                </div>
+                <div className="meta-data-item">
+                  <span className="meta-data-label">Social:</span>
+                  <div className="social-links mt-0">
+                    {user?.company_details?.twitter ? (
+                      <Link
+                        href={user?.company_details?.twitter}
+                        className="social-link"
+                        target="_blank"
+                      >
+                        <i className="bi bi-twitter"></i>
+                      </Link>
+                    ) : null}
+                    {user?.company_details?.linkedin ? (
+                      <Link
+                        href={user?.company_details?.linkedin}
+                        className="social-link"
+                        target="_blank"
+                      >
+                        <i className="bi bi-linkedin"></i>
+                      </Link>
+                    ) : null}
+                  </div>
+                </div>
+              </div>
+
+              {/* <button className="contact-button">Send Message</button> */}
+            </div>
+          )}
         </aside>
       </div>
     </div>
