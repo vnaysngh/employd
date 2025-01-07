71c230e1b9a50fc6163011dce1c85b8b8aeb95ce
vnaysngh <singhvinay2525@gmail.com>
2025-01-07 12:46:11 +0530

    roadmap section update on landingpage

--- src/app/components/roadmap/roadmap.tsx
+++ src/app/components/roadmap/roadmap.tsx
@@ -4,34 +4,67 @@ import Image from "next/image";
 import roadmap from "@/assets/images/assets/roadmap.png";
 const roadmapPhases = [
   {
+    phase: "Q1 - JAN 2025",
     title: "Launch MVP on testnet",
     description:
-      "Deploy the MVP on testnet to showcase decentralized attestations and gather feedback."
+      "Deploy the MVP to showcase decentralized attestations and gather feedback."
   },
   {
-    title: "Attestation Explorer (on testnet)",
+    phase: "Q1 - JAN 2025",
+    title: "Attestation Explorer ",
     description:
-      "Allow users to search and view verified attestations on testnet for better transparency."
+      "Allow users to search and view verified attestations for better transparency."
   },
   {
-    title: "Update Attestations (on testnet)",
+    phase: "Q1 - FEB 2025",
+    title: "Update Attestations",
     description:
       "Enable users to update their attestations for accuracy and flexibility."
   },
   {
+    phase: "Q1 - MAR 2025",
     title: "Launch on Mainnet",
-    description:
-      "Go live on mainnet with the decentralized attestation platform."
+    description: "Go live on mainnet with the platform."
   },
   {
+    phase: "Q1 - APRIL 2025",
+    title: "Real-Time Messaging & Notifications",
+    description:
+      "Add messaging and smart alerts for seamless communication between talents and employers."
+  },
+  {
+    phase: "Q1 - APRIL 2025",
     title: "Web3 Resume for talents",
     description:
       "Showcase on-chain activity, NFTs, and skills in a verified digital resume."
   },
   {
+    phase: "Q1 - MAY 2025",
     title: "Job Marketplace",
     description:
       "Connect talents and employers through verified attestations for trust-based hiring."
+  },
+  {
+    phase: "Q2-Q3 - JUNE-JULY 2025",
+    title: "Batch Attestations for Employers",
+    description:
+      "Allow employers to issue multiple attestations in one go, simplifying team verifications."
+  },
+  {
+    phase: "Q3 - SEP 2025",
+    title: "Insights Dashboard for Users",
+    description: "Offer analytics for tracking progress and hiring trends."
+  },
+  {
+    phase: "Q4 -  OCT 2025",
+    title: "Collaborative Peer Attestations",
+    description: "Enable users to validate each other's skills and experiences."
+  },
+  {
+    phase: "Q1 2026",
+    title: "Open-Source Initiative",
+    description:
+      "Make the platform open source to encourage contributions and transparency."
   }
 ];
 
@@ -40,43 +73,35 @@ const Roadmap = () => {
     <section className="text-feature-one position-relative pt-180 xl-pt-150 lg-pt-100 md-pt-80">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-lg-8 order-lg-last">
-            <div className="ps-lg-4 wow fadeInRight">
-              <div className="title-one">
-                <h2 className="fw-600">Roadmap</h2>
-              </div>
+          <div className="ps-lg-4 wow fadeInRight">
+            <div className="title-one">
+              <h2 className="fw-600 text-center">Roadmap</h2>
+            </div>
 
-              <div className="mt-40 md-mt-20 mb-40 md-mb-20 justify-content-center">
-                <div className="roadmap">
-                  <div className="roadmap-grid">
+            <div className="mt-40 md-mt-20 mb-40 md-mb-20 justify-content-center">
+              <div className="roadmap">
+                <div className="roadmap-grid" style={{ maxWidth: "none" }}>
+                  <div className="row align-items-center">
                     {roadmapPhases.map((phase, index) => (
-                      <div key={index} className="roadmap-phase">
-                        <h4 className="roadmap-phase-number">
-                          Phase {index + 1}
-                        </h4>
-                        <h3 className="roadmap-phase-title">{phase.title}</h3>
-                        <p className="roadmap-phase-description">
-                          {phase.description}
-                        </p>
+                      <div className="col-lg-4 col-md-4">
+                        <div key={index} className="roadmap-phase">
+                          <h4
+                            className="roadmap-phase-number"
+                            style={{ color: "#19aa6e" }}
+                          >
+                            {phase.phase}
+                          </h4>
+                          <h3 className="roadmap-phase-title">{phase.title}</h3>
+                          <p className="roadmap-phase-description">
+                            {phase.description}
+                          </p>
+                        </div>
                       </div>
                     ))}
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-md-8 m-auto order-lg-first">
-            <div className="img-data position-relative me-xl-5 md-mt-20">
-              <div className="row align-items-center gx-xl-5">
-                <Image src={roadmap} alt="shape" className="lazy-img" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="border-bottom pb-50 lg-pb-20 mt-90 lg-mt-30">
-          <div className="row justify-content-center">
-            {/* <CounterOne /> */}
           </div>
         </div>
       </div>
